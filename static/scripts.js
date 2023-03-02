783e60b8bac674eed977328c311d38e253861a4e
Ruff9 <Ruff9@users.noreply.github.com>
2023-03-02 15:09:41 +0000

    deploy: dc335375d40d6622a1f0a678332a9e870815a30f

--- static/scripts.js
+++ static/scripts.js
@@ -1,9 +1,12 @@
-const exercises = await getExercises();
+let currentURL = "/sept_minutes/static";
+if (location.hostname === "0.0.0.0") { currentURL = "/static"; }
+
 const playButton = document.querySelector("#play-button");
+const exercises = await getExercises();
 
 async function getExercises() {
   let data;
-  const res = await fetch('../static/exercises.json')
+  const res = await fetch(currentURL + '/exercises.json')
   data = await res.json();
 
   return data.exercises;
