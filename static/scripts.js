54f9b8e697a5c6fde20f10350ac899cd2433f683
Rémy Maucourt <remy.maucourt@yahoo.fr>
2023-03-02 15:42:07 +0100

    fix fetch issue

--- static/scripts.js
+++ static/scripts.js
@@ -3,7 +3,7 @@ const playButton = document.querySelector("#play-button");
 
 async function getExercises() {
   let data;
-  const res = await fetch('/static/exercises.json')
+  const res = await fetch('../static/exercises.json')
   data = await res.json();
 
   return data.exercises;
