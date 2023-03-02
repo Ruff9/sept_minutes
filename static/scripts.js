64bf3f1bdd5ddcc9e01a10c7df0b0d7f89cb91f2
Rémy Maucourt <remy.maucourt@yahoo.fr>
2023-03-02 16:14:59 +0100

    refactoring

--- static/scripts.js
+++ static/scripts.js
@@ -1,12 +1,10 @@
-let currentURL = "/sept_minutes/static";
-if (location.hostname === "0.0.0.0") { currentURL = "/static"; }
-
+const currentURL = location.hostname === "0.0.0.0" ? "" : "/sept_minutes";
 const playButton = document.querySelector("#play-button");
 const exercises = await getExercises();
 
 async function getExercises() {
   let data;
-  const res = await fetch(currentURL + '/exercises.json')
+  const res = await fetch(currentURL + '/static/exercises.json')
   data = await res.json();
 
   return data.exercises;
@@ -17,5 +15,3 @@ playButton.onclick = async function() {
     console.log(exo.name);
   });
 };
-
-
