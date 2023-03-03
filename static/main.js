9d72e0b5470bc55ff121e70e6104f7766289e712
Rémy Maucourt <remy.maucourt@yahoo.fr>
2023-03-03 14:55:05 +0100

    refactoring

--- static/main.js
+++ static/main.js
@@ -4,9 +4,8 @@ const exercises = await getExercises();
 import observable from "./observable.js";
 
 async function getExercises() {
-  let data;
   const res = await fetch(currentURL + '/static/exercises.json')
-  data = await res.json();
+  let data = await res.json();
   
   return data.exercises;
 }
@@ -14,12 +13,9 @@ async function getExercises() {
 const playButton = document.querySelector("#play-button");
 
 playButton.onclick = async function() {
-  startWorkout();
-};
-
-function startWorkout() {
+  document.getElementById("play-button-container").remove();
   startClock(520);
-}
+};
 
 function workout(data) {
   const position = Math.trunc(data / 40)
@@ -31,31 +27,12 @@ function workout(data) {
 function displayExercise(exercise) {
   let exerciseEl = document.getElementById("exercise-container");
   
-  if (exerciseEl === null) {
-    const container = document.querySelector("#workout-container");
-
-    exerciseEl = document.createElement("div");
-    exerciseEl.classList.add("exercise-container");
-    exerciseEl.setAttribute("id", "exercise-container");
-    container.prepend(exerciseEl)
-  }
-
   exerciseEl.innerHTML = "";
   exerciseEl.append(exercise.name);
 }
 
 function displayClock(data) {
   let timerEl = document.getElementById("timer-container");
-  
-  if (timerEl === null) {
-    const container = document.querySelector("#workout-container");
-    empty(container);
-    
-    timerEl = document.createElement("div");
-    timerEl.classList.add("timer-container");
-    timerEl.setAttribute("id", "timer-container");
-    container.append(timerEl)
-  }
 
   timerEl.innerHTML = "";
   timerEl.append(data);
@@ -66,6 +43,7 @@ observable.subscribe(displayClock);
 
 function startClock(maxTime) {
   let current = 0;
+  observable.notify(current);
 
   function add_time() {
     current++;
@@ -75,10 +53,4 @@ function startClock(maxTime) {
   };
 
   let timer = setInterval(add_time, 1000);
-}
-
-function empty(element) {
-  while(element.firstElementChild) {
-     element.firstElementChild.remove();
-  }
 }
