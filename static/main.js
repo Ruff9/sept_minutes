8e3b6464cf318777290d0d4aa263f1ab02940156
Ruff9 <Ruff9@users.noreply.github.com>
2023-03-05 16:41:46 +0000

    deploy: eb8ede43f98146c1592583c71414ca2530ca568a

--- static/main.js
+++ static/main.js
@@ -1,22 +1,18 @@
 const currentURL = location.hostname === "0.0.0.0" ? "" : "/sept_minutes";
-const exercises = await getExercises();
 let timerResolve;
 
-async function getExercises() {
-  const res = await fetch(currentURL + '/static/exercises.json')
-  let data = await res.json();
+document.addEventListener("DOMContentLoaded", () => {
+  const playButton = document.querySelector("#play-button");
   
-  return data.exercises;
-}
-
-const playButton = document.querySelector("#play-button");
-
-playButton.onclick = async function() {
-  document.getElementById("play-button-container").remove();
-  workout();
-};
+  playButton.onclick = async function() {
+    document.getElementById("play-button-container").remove();
+    workout();
+  };
+});
 
 async function workout() {
+  const exercises = await getExercises();
+
   for (let i = 0; i < exercises.length; i++) { 
     await timedRest(exercises[i])
       .then(function () { return timedExercise(exercises[i]); })
@@ -24,6 +20,13 @@ async function workout() {
   };
 
   setTimeout(playSound("congratulation.m4a"), 1500);
+}
+
+async function getExercises() {
+  const res = await fetch(currentURL + '/static/exercises.json')
+  let data = await res.json();
+
+  return data.exercises;
 }
 
 function timedExercise(exercise) {
@@ -47,16 +50,11 @@ function timedRest(exercise) {
 
 function playInfoSound(exercise) {
   function playNextExercice() {
-    const sound = new Audio(currentURL + '/static/sound/next_exercise.m4a');
-    sound.play();
-
+    playSound("next_exercise.m4a");
     setTimeout(playExerciseName, 1800);
   }
   
-  function playExerciseName() {
-    const exo_audio = new Audio(currentURL + '/static/sound/' + exercise.sound + '.m4a')
-    exo_audio.play();
-  }
+  function playExerciseName() { playSound(exercise.sound + '.m4a'); }
 
   setTimeout(playNextExercice, 1500);
 }
